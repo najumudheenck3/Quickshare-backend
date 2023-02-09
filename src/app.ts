73752dcd2517fe7b70f4436e4b390771e332b36e
najumudheen <najumudheenck3@gmail.com>
2023-02-09 13:07:14 +0530

    before hosting changes

--- src/app.ts
+++ src/app.ts
@@ -9,12 +9,28 @@ const app: Application = express();
 const port: Number = 5000;
 
 app.use(express.json());
-app.use(CORS({
-    origin: ['https://quickshare.giftto.online'],
-    methods: ['GET', 'POST','PUT','DELETE'],
-    credentials: true,
-    exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar']},
-    ))
+// app.use(CORS({
+//     origin: ['https://quickshare.giftto.online'],
+//     methods: ['GET', 'POST','PUT','DELETE'],
+//     credentials: true,
+//     exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar']},
+//     ))
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.connect.techmart.tech');
+
+  
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Pass to next layer of middleware
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  next();
+});
 
 app.use('/',UserRoute)
 app.use('/admin',AdminRoute)
