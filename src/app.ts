b0e1d505f5c4c5a20b0aa4cff05cbf69c7e3991b
najumudheen <najumudheenck3@gmail.com>
2023-02-09 14:24:17 +0530

    before hosting changes 3

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,9 @@ app.use(function (req, res, next) {
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
   // Pass to next layer of middleware
   res.setHeader('Access-Control-Allow-Credentials', 'true');
 
