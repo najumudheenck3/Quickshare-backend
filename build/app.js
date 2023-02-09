eff192951b9c8d9083765fc9eab1bcf76c35a619
najumudheen <najumudheenck3@gmail.com>
2023-02-09 19:30:56 +0530

    corssss

--- build/app.js
+++ build/app.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
 require('dotenv').config();
 const dbConnect = require('./config/db');
 const UserRoute = require('./routes/userRoute');
@@ -12,24 +11,24 @@ const AdminRoute = require('./routes/adminRoute');
 const app = (0, express_1.default)();
 const port = 5000;
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({
-    origin: ['https://www.quickshare.giftto.online'],
-    // origin: ['https://www.quickshare.giftto.online'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE',],
-    credentials: true,
-    exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar']
-}));
-// app.use(function (req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   // res.setHeader('Access-Control-Allow-Origin', 'https://www.quickshare.giftto.online');
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//   // Pass to next layer of middleware
-//   res.setHeader('Access-Control-Allow-Credentials', 'true');
-//   next();
-// });
+// app.use(CORS({
+//     origin: ['https://www.quickshare.giftto.online'],
+//     // origin: ['https://www.quickshare.giftto.online'],
+//     methods: ['GET', 'POST','PUT','DELETE',],
+//     credentials: true,
+//     exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar']},
+//     ))
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.quickshare.giftto.online');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE,HEAD');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Pass to next layer of middleware
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 app.use('/', UserRoute);
 app.use('/admin', AdminRoute);
 dbConnect;
